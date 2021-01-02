06c4394f5437c2dc7a009463783299dcadedd3fc
Alexandre Parès <IAmFrench@users.noreply.github.com>
2021-01-02 18:06:29 +0100

    try to fix the timeout (from 30s to 200ms) - not working

--- actorService/src/main/java/com/example/demo/config/CircuitBreaker.java
+++ actorService/src/main/java/com/example/demo/config/CircuitBreaker.java
@@ -1,7 +1,6 @@
 package com.example.demo.config;
 
 import io.github.resilience4j.circuitbreaker.CircuitBreakerConfig;
-import io.github.resilience4j.common.circuitbreaker.configuration.CircuitBreakerConfigCustomizer;
 import io.github.resilience4j.timelimiter.TimeLimiterConfig;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JCircuitBreakerFactory;
 import org.springframework.cloud.circuitbreaker.resilience4j.Resilience4JConfigBuilder;
@@ -11,23 +10,28 @@ import org.springframework.context.annotation.Configuration;
 
 import java.time.Duration;
 
-import static io.github.resilience4j.circuitbreaker.CircuitBreakerConfig.SlidingWindowType.COUNT_BASED;
-
 @Configuration(proxyBeanMethods = false)
 public class CircuitBreaker {
 
     @Bean
-    public CircuitBreakerConfigCustomizer defaultCustomizer() {
-
-        return CircuitBreakerConfigCustomizer
-                .of("secondService", builder -> {
-                    builder.slidingWindowSize(5);
-                    builder.failureRateThreshold(3);
-                    builder.waitDurationInOpenState(Duration.ofSeconds(10));
-                    builder.slowCallDurationThreshold(Duration.ofSeconds(5));
-                    builder.slowCallRateThreshold(100);
-                    builder.permittedNumberOfCallsInHalfOpenState(2);
-                    builder.slidingWindowType(COUNT_BASED);
-                });
+    public Customizer<Resilience4JCircuitBreakerFactory> defaultCustomizer() {
+        return factory -> factory.configureDefault(id -> new Resilience4JConfigBuilder(id)
+                .timeLimiterConfig(TimeLimiterConfig.custom().timeoutDuration(Duration.ofSeconds(3)).build())
+                .circuitBreakerConfig(CircuitBreakerConfig.ofDefaults())
+                .build());
     }
+
+//    public CircuitBreakerConfigCustomizer getCircuitBreakerConfig() {
+//
+//        return CircuitBreakerConfigCustomizer
+//                .of("secondService", builder -> {
+//                    builder.slidingWindowSize(5);
+//                    builder.failureRateThreshold(3);
+//                    builder.waitDurationInOpenState(Duration.ofSeconds(10));
+//                    builder.slowCallDurationThreshold(Duration.ofSeconds(5));
+//                    builder.slowCallRateThreshold(100);
+//                    builder.permittedNumberOfCallsInHalfOpenState(2);
+//                    builder.slidingWindowType(COUNT_BASED);
+//                });
+//    }
 }
